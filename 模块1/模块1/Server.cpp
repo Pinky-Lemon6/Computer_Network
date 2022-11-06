6f69697d1b8f4e0e87b91d962a16fabfbf115a39
lishiyu <1359678951@qq.com>
2022-11-06 18:02:58 +0800

    feat:"基本最终实现"

--- 模块1/模块1/Server.cpp
+++ 模块1/模块1/Server.cpp
@@ -1,10 +1,87 @@
-#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
 #include "winsock2.h"
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
 #pragma comment(lib,"ws2_32.lib")
+
+
+//定位文件名后缀
+char* file_type_addr(char* argue) {
+	char* temp;
+	if ((temp = strchr(argue, '.')) != NULL) {
+		return temp + 1;
+	}
+	return (char*)"";
+}
+
+//构造并发送响应报文头部
+void send_head(char* argue, SOCKET s, char* filename) {
+	/*解析文件*/
+	char* filetype = file_type_addr(argue);
+	char* content_type = (char*)"text/plain";
+	char* body_length = (char*)"Content-Length：";
+
+	if (!strcmp(filetype, "html")) {
+		content_type = (char*)"text/html";
+	}
+	else if (!strcmp(filetype, "gif")) {
+		content_type = (char*)"image/gif";
+	}
+	else if (!strcmp(filetype, "jpg")) {
+		content_type = (char*)"image/jpg";
+	}
+	else if (!strcmp(filetype, "png")) {
+		content_type = (char*)"iamge/png";
+	}
+
+
+	/*构造响应报文头部*/
+	char* head = (char*)"HTTP/1.1 200 OK\r\n";
+	char* not_found = (char*)"HTTP/1.1 404 NOT FOUND\r\n";
+	char temp[50] = "Content-type：";
+
+
+	/*获取文件并发送*/
+	FILE* pfile;
+	errno_t error = fopen_s(&pfile, filename, "rb");
+	if (error) {
+		send(s, not_found, strlen(not_found), 0);
+	}
+	else if (send(s, head, strlen(head), 0) == -1) {
+		cout << "Sending Error!" << endl;
+		return;
+	}
+	if (content_type) {
+		strcat_s(temp, strlen(temp) + strlen(content_type) + 1, content_type);
+		strcat_s(temp, strlen(temp) + 5, "\r\n");
+
+		if (send(s, temp, strlen(temp), 0) == -1) {
+			cout << "Sending Error!" << endl;
+			return;
+		}
+	}
+	send(s, "\r\n", 2, 0);
+}
+
+//发送文件给客户端
+void send_file(SOCKET s, char* filename) {
+	int flag;
+	FILE* pfile;
+	errno_t error = fopen_s(&pfile, filename, "rb");
+
+	if (error) {
+		cout << "Fail to open file!" << endl;
+		return;
+	}
+	fseek(pfile, 0L, SEEK_END);
+	int file_len = ftell(pfile);
+	char* p = (char*)malloc(file_len + 1);
+	fseek(pfile, 0L, SEEK_SET);
+	fread(p, file_len, 1, pfile);
+	send(s, p, file_len, 0);
+}
 
 void main() {
 	WSADATA wsaData;
@@ -37,13 +114,13 @@ void main() {
 	/*监听socket*/
 	int port = 0;
 	char inaddr[20] = "";
-	char filename[48] = "";
+	char Home[48] = "";
 	cout << "Please input port number:" << endl;
 	cin >> port;
 	cout << "Please input listening address:" << endl;
 	cin >> inaddr;
 	cout << "Please input Home directory:" << endl;
-	cin >> filename;
+	cin >> Home;
 
 	/*设置服务器的端口与地址*/
 	struct sockaddr_in addr;
@@ -52,7 +129,7 @@ void main() {
 	addr.sin_addr.s_addr = inet_addr(inaddr);  //处理监听地址
 
 	/*binding*/
-	int rtn = bind(srvSocket, (LPSOCKADDR) & addr, sizeof(addr));
+	int rtn = bind(srvSocket, (LPSOCKADDR)&addr, sizeof(addr));
 	if (rtn != SOCKET_ERROR)
 		cout << "Socket bind OK!" << endl;
 
@@ -67,22 +144,22 @@ void main() {
 
 	while (true) {
 		/*等待连接，创建会话socket*/
-		SOCKET sessionSocket = accept(srvSocket,(LPSOCKADDR) & clientAddr, &clientAddrLen);
+		SOCKET sessionSocket = accept(srvSocket, (LPSOCKADDR)&clientAddr, &clientAddrLen);
 		if (sessionSocket != INVALID_SOCKET)
 			cout << "Socket listen one client request!" << endl;
-		
+
 		cout << "The IP and port number of the client are：" << inet_ntoa(clientAddr.sin_addr) << htons(clientAddr.sin_port) << endl;
-		
+
 		/*开始会话*/
-		char recvBuf[4096]; //设置缓冲区
-		rtn = recv(sessionSocket, recvBuf, 1024, 0);
+		char recvBuf[4096] = ""; //设置缓冲区
+		rtn = recv(sessionSocket, recvBuf, 4096, 0);
 		if (rtn != SOCKET_ERROR)
-			cout << "Received "<<strlen(recvBuf)<<" bytes from client:"<<recvBuf<< endl;
+			cout << recvBuf << "Received " << strlen(recvBuf) << " bytes of data from client." << endl;
 
 		/*处理输入的文件名*/
 		char name[30] = "";
-		
-		int i = find(recvBuf, recvBuf+strlen(recvBuf), '/')-recvBuf;
+
+		int i = find(recvBuf, recvBuf + strlen(recvBuf), '/') - recvBuf;
 		int j = 0;
 		while (recvBuf[i + 1] != ' ') {
 			name[j] = recvBuf[i + 1];
@@ -94,17 +171,26 @@ void main() {
 
 		/*处理文件路径*/
 		char path[80] = "";
-		strcpy(path, filename);
-		strcat(path, name);
+		if (Home[strlen(Home) - 1] != '\\') {
+			Home[strlen(Home)] = '\\';
+		}
+		strcpy_s(path, sizeof(path) / sizeof(path[0]), Home);
+		strcat_s(path, strlen(path) + strlen(name) + 1, name);
 		cout << "File path is：" << path << endl;
 
+		/*发送响应报文头部及文件*/
+		send_head(path, sessionSocket, path);
+		send_file(sessionSocket, path);
 
-
+		/*关闭sessionSocket*/
+		closesocket(sessionSocket);
 	}
 
+	/*关闭srvSocket*/
+	closesocket(srvSocket);
+	WSACleanup();
 
-
-
-
-
+	getchar();
+	return;
 }
+
