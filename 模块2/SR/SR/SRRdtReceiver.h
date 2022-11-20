03374c914eee3617af57c6930cad24203ad5eca0
lishiyu <1359678951@qq.com>
2022-11-20 16:48:09 +0800

    feat:"初始化项目，引入所需使用的头文件，创建对应源文件"

---
+++ 模块2/SR/SR/SRRdtReceiver.h
@@ -0,0 +1,21 @@
+#ifndef STOP_WAIT_RDT_RECEIVER_H
+#define STOP_WAIT_RDT_RECEIVER_H
+#include "RdtReceiver.h"
+class SRRdtReceiver :public RdtReceiver
+{
+private:
+	int base;
+	int NextSeqNum;	// 期待收到的下一个报文序号
+	Packet lastAckPkt;				//上次发送的确认报文
+	Packet ReceivedPacket[Winlength];		//已发送并等待Ack的数据包
+	bool packetWaitingStatus[Winlength];  //各数据包的等待状态
+public:
+	SRRdtReceiver();
+	virtual ~SRRdtReceiver();
+
+public:
+
+	void receive(const Packet& packet);	//接收报文，将被NetworkService调用
+};
+
+#endif
