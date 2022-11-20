5f4bd5032e8a3e18defd7d8f9c92232752ea7d44
lishiyu <1359678951@qq.com>
2022-11-20 22:40:03 +0800

    feat:"初始化TCP项目，引入所需头文件"

---
+++ 模块2/TCP/TCP/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#define Winlength 8 //窗口大小
+#define N 4 //序号二进制位数
+// TODO: 在此处引用程序需要的其他头文件
+#pragma comment (lib,"D:\\Study\\ComputerNetwork\\Module2\\netsimlib.lib")
+
+#include <iostream>
+using namespace std;
+
+//#pragma warning(disable:4482)
